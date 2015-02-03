6440bd3f5e29f8fbc5d0739d6d9aa367af5ff8bb
falfaro05 <f.alfaro05@hotmail.com>
2015-02-03 12:00:15 -0600

    Interactuando JQuery

---
+++ js/miScript.js
@@ -0,0 +1,37 @@
+$(document).on("ready", inicio);
+
+function inicio(){
+
+
+	$("#publicar").on("click", mostrarOcultar);
+	$("#formulario").on("submit", agregarPost);
+}
+
+function mostrarOcultar(){
+
+	var formulario = $("#formulario");
+	formulario.slideToggle();
+	$("nav").slideToggle();
+
+}
+
+function agregarPost(event){
+
+	event.preventDefault();
+
+	var titulo = $("#titulo").val();
+	var clon = $(".item:first").clone();
+	var link = $("#link").val();
+	var contenedor = $("#contenido");
+	
+	clon.find("h2 a").text(titulo);
+	clon.find("h2 a").attr("href", link);
+
+	clon.hide();
+
+	contenedor.prepend(clon);
+	mostrarOcultar();
+
+	clon.fadeIn();
+}
+
